7cb4195883590725a732c168cead553e1055f1a5
Patrick McSweeny <patmcsweeny@gmail.com>
2018-10-22 22:13:20 -0400

    Fix featured solution

--- spec/dashboard_spec.rb
+++ spec/dashboard_spec.rb
@@ -4,14 +4,11 @@ require "dashboard"
 describe Dashboard do
   describe "#posts" do
     it "returns posts created today" do
-      create :post, title: "first_today", created_at: Time.now.beginning_of_day
-      create :post, title: "last_today", created_at: Time.now.end_of_day
-      create :post, title: "yesterday", created_at: 1.day.ago.end_of_day
+      posts_published_today = double("published_today")
+      expect(Post).to receive(:today).and_return(posts_published_today)
       dashboard = Dashboard.new(posts: Post.all)
 
-      result = dashboard.todays_posts
-
-      expect(result.map(&:title)).to match_array(%w(first_today last_today))
+      expect(dashboard.todays_posts).to eq posts_published_today
     end
   end
 
